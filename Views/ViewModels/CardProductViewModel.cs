3b0bbe888d6e6500bad419ee8e28c50f2afc0fab
erogluibrahim <ibrahimeroglu99@gmail.com>
2023-08-18 17:54:16 +0300

    CartProduct ViewModel oluşturuldu ve 3 product nesnesi cardproduct nesnesine eklendi

---
+++ Views/ViewModels/CardProductViewModel.cs
@@ -0,0 +1,9 @@
+﻿using DersModelYapisi.Models;
+
+namespace DersModelYapisi.Views.ViewModels
+{
+    public class CardProductViewModel
+    {
+        public List<CartProduct> products { get; set; }
+    }
+}
